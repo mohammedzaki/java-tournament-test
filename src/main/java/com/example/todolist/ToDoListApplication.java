318c9846f82038f08f156d102abe3cc35cfa595c
Mohammed Zaki <mohammedzaki.dev@gmail.com>
2018-03-18 07:08:44 +0200

    update app properties

--- src/main/java/com/example/todolist/ToDoListApplication.java
+++ src/main/java/com/example/todolist/ToDoListApplication.java
@@ -1,12 +1,23 @@
 package com.example.todolist;
 
+import javax.persistence.EntityManagerFactory;
+import org.hibernate.SessionFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ToDoListApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ToDoListApplication.class);
+    }
+    
+    @Bean
+    public SessionFactory sessionFactory(EntityManagerFactory emf) {
+        if (emf.unwrap(SessionFactory.class) == null) {
+            throw new NullPointerException("factory is not a hibernate factory");
+        }
+        return emf.unwrap(SessionFactory.class);
     }
 }
